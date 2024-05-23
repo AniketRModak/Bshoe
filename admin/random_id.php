1f284392fe5ad4b0b6d16c37325a8cc4e9910731
Aniket <modakaniket2000@gmail.com>
2024-05-23 16:38:21 +0530

    Create a git repo

---
+++ admin/random_id.php
@@ -0,0 +1,20 @@
+<?php
+/* Confirmation Code */
+function createRandomPassword()
+{
+    $chars = "01234567899876543210";
+    srand((double) microtime() * 1000000);
+    $i = 0;
+    $pass = '';
+    while ($i <= 7) {
+        $num = rand() % 33;
+        $tmp = substr($chars, $num, 1);
+        $pass = $pass . $tmp;
+        $i++;
+    }
+    return $pass;
+}
+
+/*values*/
+$code = createRandomPassword();
+?>
